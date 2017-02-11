e9f80887403e17ece09e455bfac76775e262a02b
dhakrasp <pranav.dhakras@gmail.com>
2017-02-11 14:27:00 +0530

    corrections

--- python/read_json.py
+++ python/read_json.py
@@ -1,11 +1,11 @@
 import json
-import pprint
 import nltk.tokenize
 import pickle
 import os
 import codecs
 
-def get_next_id(init_seed = -1, seed_file='seed'):
+
+def get_next_id(seed_file, init_seed=-1):
 	if os.path.exists(seed_file):
 		with open(seed_file, 'r') as f:
 			seed = pickle.load(f)
@@ -19,17 +19,23 @@ def get_next_id(init_seed = -1, seed_file='seed'):
 	return seed
 
 if __name__ == '__main__':
-	with codecs.open('../data/BioASQ-training4b/BioASQ-trainingDataset4b.json') as corpus_file:
+	with codecs.open('../data/BioASQ-training4b/BioASQ-trainingDataset4b.json', encoding = 'utf-8') as corpus_file:
 	    data = json.load(corpus_file)
 	x = data["questions"]
-	init_seed = -1
-	seed_file = 'seed'
-	with codecs.open('data.txt', 'a') as f:
+
+	# Dump ideal answers
+	with codecs.open('data.txt', 'a', encoding = 'utf-8') as f:
 		for i in x:
 			para = i['ideal_answer'][0]
 			sentences = nltk.tokenize.sent_tokenize(para)
 			for s in sentences:
-				print(s)
-				line = '{}{}{}'.format(get_next_id(), '\t', s)
+				line = u'{}{}{}'.format(get_next_id(seed_file='answers_seed'), '\t', s)
 				f.write(line+'\n')
-			break
+			# break
+
+	# Dump questions
+	with codecs.open('questions.txt', 'a', encoding = 'utf-8') as f:
+		for i in x:
+			question = i['body']
+			line = u'{}{}{}'.format(get_next_id(seed_file='questions_seed'), '\t', question)
+			f.write(line+'\n')
